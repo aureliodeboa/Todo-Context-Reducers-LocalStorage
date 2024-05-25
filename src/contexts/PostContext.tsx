74e03599d536f15c848f5f61ca14d45e93dfc865
Aurelio Ribeiro <aurelio74123@gmail.com>
2024-05-25 09:15:00 -0300

    style

--- src/contexts/PostContext.tsx
+++ src/contexts/PostContext.tsx
@@ -15,12 +15,7 @@ export const PostProvider = ({children} : {children: ReactNode}) =>{
     const [posts,setPosts]= useState<Post[]>([]);
 
     const addPost = (title:string, body:string)=>{
-        setPosts([
-            ...posts,
-            {
-                id: posts.length, title, body
-            }
-        ])
+        setPosts([...posts,{ id: posts.length, title, body  }])
     }
     return(
         <PostContext.Provider value={{ posts, addPost }}> {children}</PostContext.Provider>
